36a063778948b1d4407f3cc7491385a2096b2477
Adossi Fred William <f.adossi@alustudent.com>
2024-11-10 20:26:16 +0200

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""Saves command line arguments to a file."""
-import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+from sys import argv
+"""access commandline arguments"""
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""create object from JSON file"""
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""writes an object to text file, using JSON representation"""
 
+filename = "add_item.json"
 try:
-    args = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    args = []
-args.extend(sys.argv[1:])
-save_to_json_file(args, "add_item.json")
+    content = load_from_json_file(filename)
+except:
+    content = []
+
+for i in range(1, len(argv)):
+    content.append(argv[i])
+save_to_json_file(content, filename)
