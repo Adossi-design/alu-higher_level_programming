30389d1915b9fa286965459a4f940e3cae0bb5db
Adossi Fred William <f.adossi@alustudent.com>
2024-11-12 18:07:33 +0200

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-"""
-more class base
-"""
+"""This module inherits from the list class"""
 
 
-Rectangle = __import__('9-rectangle').Rectangle
-
-
-"""
-Square class
-"""
-
-
-class Square(Rectangle):
-    """ Square Class """
-    def __init__(self, size):
-        """ instantiation with size """
-        self.__size = size
-        super().__init__(self.__size, self.__size)
+class MyList(list):
+    """A class that inherits from list"""
+    def print_sorted(self):
+        """prints a sorted list"""
+        print(sorted(self))
