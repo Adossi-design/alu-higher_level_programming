0069ef99ac98a16f1a996d76b9eb0f2e5e64e345
Adossi Fred William <f.adossi@alustudent.com>
2024-11-19 23:47:10 +0200

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-School-User-Id: 98" "$1""
+# send a POST request to the passed URL using curl, and display the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
