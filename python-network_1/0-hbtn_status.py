8d99def2db97eceb083d59d53bbd2dcba4de1aa4
Adossi Fred William <f.adossi@alustudent.com>
2024-11-29 08:55:07 +0200

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,11 +2,13 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    
+    url = "https://intranet.hbtn.io/status"
+    # Uncomment the line below to fetch from the alternative URL
+    # url = "http://0.0.0.0:5050/status"
+
     with urllib.request.urlopen(url) as response:
         body = response.read()
-        
+
         print("Body response:")
         print(f"    - type: {type(body)}")
         print(f"    - content: {body}")
